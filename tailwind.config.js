71de7269dd3b20017c21179c397ad1495487df55
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-02-21 07:15:43 -0500

    instalacion

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      ...colors,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
