3951e4c94db39195695e86709e38b1cebec2e8e4
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-02-25 07:26:30 -0500

    login

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,6 +1,15 @@
 import { LockClosedIcon } from "@heroicons/react/solid";
+import { useRef } from "react";
 
 export default function LoginPage() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(email, password);
+  };
   return (
     <>
       <div className="flex items-center justify-center min-h-full px-4 py-12 sm:px-6 lg:px-8">
@@ -15,14 +24,18 @@ export default function LoginPage() {
               Sign in to your account
             </h2>
           </div>
-          <Form className="mt-8 space-y-6" action="#" method="POST">
+          <form
+            className="mt-8 space-y-6"
+            onSubmit={submitHandler}
+            method="POST"
+          >
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="-space-y-px rounded-md shadow-sm">
               <div>
                 <label htmlFor="email-address" className="sr-only">
                   Email address
                 </label>
-                <Field
+                <input
                   id="email-address"
                   name="email"
                   type="email"
@@ -30,13 +43,14 @@ export default function LoginPage() {
                   required
                   className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Email address"
+                  ref={emailRef}
                 />
               </div>
               <div>
                 <label htmlFor="password" className="sr-only">
                   Password
                 </label>
-                <Field
+                <input
                   id="password"
                   name="password"
                   type="password"
@@ -44,6 +58,7 @@ export default function LoginPage() {
                   required
                   className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Password"
+                  ref={passwordRef}
                 />
               </div>
             </div>
@@ -88,7 +103,7 @@ export default function LoginPage() {
                 Sign in
               </button>
             </div>
-          </Form>
+          </form>
         </div>
       </div>
     </>
